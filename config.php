356948cebdf3b74ab869cae45d02721d4ffc6061
thiago25104708360023-prog <thiago.25104708360023@faeterj-rio.edu.br>
2026-04-13 11:25:46 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,24 @@
 <?php
-$arquivoperguntas = "perguntas.txt";
-$arquivousuarios = "usuarios.txt";
+$arquivo_perguntas = 'perguntas.txt';
+$arquivo_usuarios = 'usuarios.txt';
+
+function lerPerguntas() {
+    global $arquivo_perguntas;
+    if (!file_exists($arquivo_perguntas)) return [];
+    $linhas = file($arquivo_perguntas, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $dados = [];
+    foreach ($linhas as $linha) {
+        $dados[] = explode('|', $linha);
+    }
+    return $dados;
+}
+
+function salvarPerguntas($perguntas) {
+    global $arquivo_perguntas;
+    $conteudo = "";
+    foreach ($perguntas as $p) {
+        $conteudo .= implode('|', $p) . PHP_EOL;
+    }
+    file_put_contents($arquivo_perguntas, $conteudo);
+}
+?>
